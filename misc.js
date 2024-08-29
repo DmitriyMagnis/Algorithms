22b36b97143b2bd2e3e70f39a2fd31e41f15d790
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-08-29 23:28:44 +0200

    added get func

--- misc.js
+++ misc.js
@@ -352,20 +352,65 @@ const promise = (t, data) => new Promise(r => setTimeout(() => r(data), t))
 
 // console.log(func(str, substr)); //[0, 5]
 
-function checkPalindrom(str) {
-	return str == str.split('').reverse().join('')
+// function checkPalindrom(str) {
+// 	return str == str.split('').reverse().join('')
+// }
+
+// function checkPalindrom2(str) {
+// 	let j = str.length - 1
+// 	for (let i = 0; i < str.length / 2; i++) {
+// 		if (str[i] != str[j]) {
+// 			return false
+// 		}
+// 		j--
+// 	}
+// 	return true
+// }
+
+// console.log(checkPalindrom('aba'))
+// console.log(checkPalindrom2('aba'))
+
+// const get = (obj, path) => {
+//   const queue = path.split('.');
+//   let cNode = { ...obj };
+//   while (queue.length) {
+//     const step = queue.shift();
+
+//     if (step in cNode) {
+//       cNode =
+//         cNode[step]?.constructor === Object ? { ...cNode[step] } : cNode[step];
+
+//       if (queue.length === 0) {
+//         return cNode;
+//       }
+//     } else {
+//       return false;
+//     }
+//   }
+// };
+// const get = (obj, path) => {
+// 	const tokens = path.split('.');
+// 	if (tokens.length === 1) {
+// 		return obj[path]
+// 	}
+
+// 	for (const token of tokens) {
+// 		if (typeof obj[token] === 'object') {
+// 			return get(obj[token], tokens.slice(1).join('.'))
+// 		}
+// 	}
+// }
+
+const obj = {
+	a: {
+		b: {
+			c: null,
+		},
+		e: 'f',
+	},
 }
 
-function checkPalindrom2(str) {
-	let j = str.length - 1
-	for (let i = 0; i < str.length / 2; i++) {
-		if (str[i] != str[j]) {
-			return false
-		}
-		j--
-	}
-	return true
-}
-
-console.log(checkPalindrom('aba'))
-console.log(checkPalindrom2('aba'))
+console.log(get(obj, 'a.b'))
+console.log(get(obj, 'a.b.c'))
+console.log(get(obj, 'a.e'))
+console.log(get(obj, 'a.x.e'))
